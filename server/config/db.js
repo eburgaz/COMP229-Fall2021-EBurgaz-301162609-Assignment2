4c8c92e822a6be280d165df81a40c8e1adf51ac7
eburgaz <90803369+eburgaz@users.noreply.github.com>
2021-10-22 13:58:51 -0400

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business-contact"
-
+    //"URI": "mongodb://localhost/business-contact"
+    "URI": "mongodb+srv://admin:tzYUAjfnONauvglL@cluster-comp229.nhrrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     
 }
